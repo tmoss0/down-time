fb2770a2ab8f354f7b27d994ea8f963f376ac974
tmoss0 <tmoss726@hotmail.com>
2020-11-22 20:26:42 -0500

    Update: blog page added

---
+++ src/pages/blog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
+/**
+ * File: blog.js
+ * Description: the blog page renders each blog post using the blog
+ *              template created under templates/blog.js
+ * 
+ * Date: 11/22/20
+ * 
+ */
+
+const Blog = () => {
+    
+}
+
+export default Blog;
